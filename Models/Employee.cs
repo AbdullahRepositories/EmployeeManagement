7a6140573db989c6e4abe49c70e9c5a143721716
abdullah <abdullah@E-LENOVO-LAPTOP>
2024-01-20 23:22:45 +0300

    finish the validation for the Employee class

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -6,9 +6,17 @@ namespace EmployeeManagement.Models
     {
         public int Id { get; set; }
         [Required]
+        [MaxLength(50,ErrorMessage ="Name Cannot exeed 50 characters")]
         public string Name { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",ErrorMessage ="Invalid Email Format")]
+        [Display(Name = "Office Email")]
         public string Email{ get; set; }
-        public Dept Department { get; set; }
+        /// <summary>
+        /// by adding the required annotation for enums, it wouldnot work as enums already have default values. which means if you add other values than the specified ones in the num attributes, it would pop  an exception.
+        /// but by making the enum nullable, the "Required" annotaion would work perfectley if there is an empty input and the enum exception woudnot appear because its already nullabel
+        /// </summary>
+        [Required]
+        public Dept? Department { get; set; }
     }
 }
